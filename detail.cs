a9bdf9494960f9ae2d8fd36f73293b0abbf644f1
SuchGlasha <such.glasha@gmail.com>
2015-10-18 14:02:00 +0300

    Create detail.cs

---
+++ detail.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public  class Detail
+    {
+        public double Amount { get; set; }
+
+        public  string Category { get; set; }
+
+        public  DateTime Date { get; set; }
+
+        public  string Transaction { get; set; }
+
+
+        public static Detail NewDatail(double MYamount, string MYcategory, DateTime MYdate, string MYtransaction)
+        {
+            Detail myNewDetail = new Detail {Amount = MYamount, Category = MYcategory, Transaction = MYtransaction, Date = MYdate};
+
+
+            return myNewDetail;
+
+        }
+
+    }
+
+}
