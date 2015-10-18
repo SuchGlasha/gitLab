f150d226efdb8d05ddf946afc05d86c760e637e8
KatePrish <kiti_9595@mail.ru>
2015-10-18 16:20:49 +0300

    изменение типа переменной MYTransaction
    string на int

--- detail.cs
+++ detail.cs
@@ -15,10 +15,10 @@ namespace ConsoleApplication1
 
         public  DateTime Date { get; set; }
 
-        public  string Transaction { get; set; }
+        public  int Transaction { get; set; }
 
 
-        public static Detail NewDatail(double MYamount, string MYcategory, DateTime MYdate, string MYtransaction)
+        public static Detail NewDatail(double MYamount, string MYcategory, DateTime MYdate, int MYtransaction)
         {
             Detail myNewDetail = new Detail {Amount = MYamount, Category = MYcategory, Transaction = MYtransaction, Date = MYdate};
 
